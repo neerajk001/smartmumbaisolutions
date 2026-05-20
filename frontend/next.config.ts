6fca012aa54edcc58b592cd596d7655d4512b3e6
[Neeraj kushwaha] <[workwithneeraj.01@gmail.com]>
2026-05-20 08:32:11 +0530

    next config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -18,6 +18,18 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/admin/:path*',
+        destination: 'http://127.0.0.1:7001/api/admin/:path*',
+      },
+      {
+        source: '/api/gallery/:path*',
+        destination: 'http://127.0.0.1:7001/api/gallery/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
